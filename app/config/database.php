d4d89e565dd8093138b80b88d3c77cb82cc6dfc2
Abdur Rahman <rubel100@gmail.com>
2014-12-21 01:05:24 -0500

    added first migration to create categories table

--- app/config/database.php
+++ app/config/database.php
@@ -55,12 +55,12 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'ecommerce',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'prefix'    => 'ec_',
 		),
 
 		'pgsql' => array(
---
+++ app/database/migrations/2014_12_21_054902_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCategoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('categories', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('parent_id');
+			$table->string('name');
+			$table->boolean('status');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('categories');
+	}
+
+}
